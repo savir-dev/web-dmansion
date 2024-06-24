5ac0ebd63ca2c96de340c95a131f718c8d702e7a
fchyoga <fchyoga@gmail.com>
2024-06-24 13:20:43 +0700

    baru

---
+++ cms-wordpress-app/next.config.js
@@ -0,0 +1,32 @@
+if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `);
+}
+
+const { protocol, hostname, port, pathname } = new URL(
+  process.env.WORDPRESS_API_URL,
+);
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '1.gravatar.com',
+        port,
+        pathname: `${pathname}/**`,
+      },
+    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: protocol.slice(0, -1),
+    //     hostname,
+    //     port,
+    //     pathname: `${pathname}/**`,
+    //   },
+    // ],
+  },
+};
